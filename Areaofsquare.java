631f176c2ba4ecb7b809e5726359a7d9a028a1ef
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 20:16:32 +0530

    area of sqaure program

---
+++ Areaofsquare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Areaofsquare{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter side");
+        int side=obj.nextInt();
+        int area=side*side;
+        System.out.println(area);
+    }
+}
