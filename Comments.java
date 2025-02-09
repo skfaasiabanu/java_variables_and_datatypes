1ab404e6ccb756c67fbc6f8b0cd1e4b14e90f5c1
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 21:21:14 +0530

    comment

---
+++ Comments.java
@@ -0,0 +1,15 @@
+public class comments{
+    public static void main(String args[])
+    {  
+        //calculating sum of 2 numbers
+    
+    /* calculating sum of 2 numbers
+    hello
+    world
+    */
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
