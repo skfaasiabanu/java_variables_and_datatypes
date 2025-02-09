a0da69103c031a1360a02db76891bf9012094e74
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 21:23:47 +0530

    data tpes

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes
+{
+    public static void main(String args[])
+    {
+        byte b=8;
+        System.out.println(a);
+        char ch='a';
+        System.out.println(ch);
+        boolean var=false;
+        System.out.println(var);
+        float price=10.5;//double
+        System.out.println(price);
+        int number=20;//long
+        System.out.println(number);
+        short d=240;
+        
+    }
+}
