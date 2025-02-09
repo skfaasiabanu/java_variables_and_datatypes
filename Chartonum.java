f0ecb6f4b1d21bf206a3996856dc64f9cb8f2dc3
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 21:18:32 +0530

    char to num

---
+++ Chartonum.java
@@ -0,0 +1,14 @@
+public class Chartonum{
+    public static void main(String args[])
+    {  
+        char ch='a';
+        int number=ch;
+        System.out.println(number);
+        //number cannot be converted to character
+       /*int number=97;
+       char ch=number;
+       System.out.println(ch);
+       */
+
+    }
+}
